6125f1d2980ffd7495b5cf883b589f3ed41f3f64
richardsis <richard.moisesleon@gmail.com>
2018-09-29 11:23:21 -0500

    Creacion del proyecto

--- SIGC/src/sigc/SIGC.java
+++ SIGC/src/sigc/SIGC.java
@@ -14,6 +14,8 @@ public class SIGC {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
