a89e0a7c3ee54350bc3b95abd1c5c40a504e2c86
CESDE <CESDE@PC-34>
2018-10-12 10:49:39 -0500

    modificando por katiuska y diego

--- SIGC/src/Beans/RolBE.java
+++ SIGC/src/Beans/RolBE.java
@@ -1,64 +1,70 @@
 package Beans;
 
-
+/*
+*
+*MODIFICADO POR KATIUSKA Y DIEGO
+*
+*/
 import java.util.Date;
 
+public class RolBE {
 
-public class RolBE
- { 
-private int IndOpSp;
- private int idrol;
- private String nombrerol;
- private boolean estado;
+    private int IndOpSp;
+    private int idrol;
+    private String nombrerol;
+    private boolean estado;
 
-public RolBE(){
-this.IndOpSp = 0;
-this.idrol = 0;
-this.nombrerol = "";
-this.estado = false;
+    public RolBE() {
+        this.IndOpSp = 0;
+        this.idrol = 0;
+        this.nombrerol = "";
+        this.estado = false;
 
-}
-public RolBE(int pIndOpSp,int pidrol,String pnombrerol,boolean pestado)
-
-{
-this.IndOpSp = pIndOpSp;
-this.idrol = pidrol;
-this.nombrerol = pnombrerol;
-this.estado = pestado;
-}
- public int getIndOpSp() {
-    return IndOpSp;
     }
 
-public int getIdrol() {
-return idrol;
-
- }
-public String getNombrerol() {
-return nombrerol;
-
- }
-public boolean isEstado() {
-return estado;
-
- }
- public void setIndOpSp(int IndOpSp) {
-     this.IndOpSp = IndOpSp;
+    public RolBE(int pIndOpSp, int pidrol, String pnombrerol, boolean pestado) {
+        this.IndOpSp = pIndOpSp;
+        this.idrol = pidrol;
+        this.nombrerol = pnombrerol;
+        this.estado = pestado;
     }
 
-public void setIdrol(int idrol){
+    public int getIndOpSp() {
+        return IndOpSp;
+    }
 
-this.idrol = idrol;
-}
+    public int getIdrol() {
+        return idrol;
 
-public void setNombrerol(String nombrerol){
+    }
 
-this.nombrerol = nombrerol;
-}
+    public String getNombrerol() {
+        return nombrerol;
 
-public void setEstado(boolean estado){
+    }
 
-this.estado = estado;
-}
+    public boolean isEstado() {
+        return estado;
+
+    }
+
+    public void setIndOpSp(int IndOpSp) {
+        this.IndOpSp = IndOpSp;
+    }
+
+    public void setIdrol(int idrol) {
+
+        this.idrol = idrol;
+    }
+
+    public void setNombrerol(String nombrerol) {
+
+        this.nombrerol = nombrerol;
+    }
+
+    public void setEstado(boolean estado) {
+
+        this.estado = estado;
+    }
 
 }
